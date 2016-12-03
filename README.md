0f475f9782ac68dbeccfce71fdc976aec857fb84
Yoshihiro MIYAI <msparrow17@gmail.com>
2016-12-03 20:36:24 +0900

    Add events library sample code

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mbed-os-events-test
 mbed mbed-events library test program
+
+## Install mbed CLI
+
+See [mbed CLI](https://github.com/ARMmbed/mbed-cli#installing-mbed-cli).
+
+## Import
+
+```shell
+$ mbed import https://github.com/mia-0032/mbed-os-events-test
+```
+
+## Compile
+
+```shell
+$ mbed compile --toolchain GCC_ARM --target ${YOUR_MBED}
+```
--- main.cpp
+++ main.cpp
@@ -1,13 +1,34 @@
 #include "mbed.h"
 
 DigitalOut led1(LED1);
+InterruptIn sw1(p18);
 
-// main() runs in its own thread in the OS
-// (note the calls to Thread::wait below for delays)
-int main() {
-    while (true) {
-        led1 = !led1;
-        Thread::wait(500);
-    }
+// イベントキューを作成する
+EventQueue queue;
+
+// LEDのON/OFFを切り替える関数
+void toggle_led() {
+    led1 = !led1;
 }
 
+// タクトスイッチが押されたときに実行される関数
+void pushed() {
+    // 即実行されるイベント
+    queue.call(&toggle_led);
+    // 2000ms後に実行されるイベント
+    queue.call_in(2000, &toggle_led);
+}
+
+int main() {
+    // スイッチピンをPullUpにする
+    sw1.mode(PullUp);
+
+    // タクトスイッチが押されたときの割り込み処理
+    sw1.fall(&pushed);
+
+    // 1000msごとに定期実行するイベント
+    queue.call_every(1000, &toggle_led);
+
+    // イベントループが廻り続ける
+    queue.dispatch();
+}
